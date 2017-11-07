05b419e071fc71dc932746b9eaad8454ba966762
Marcus Heese <marcus.heese@gmail.com>
2017-11-06 21:24:44 -0800

    making types public

--- src/lib.rs
+++ src/lib.rs
@@ -5,77 +5,78 @@ use std::ptr;
 //use libc::c_uchar;
 
 #[allow(non_camel_case_types)]
-type CK_BYTE = u8;
+pub type CK_BYTE = u8;
 #[allow(non_camel_case_types)]
-type CK_CHAR = CK_BYTE;
+pub type CK_CHAR = CK_BYTE;
 #[allow(non_camel_case_types)]
-type CK_UTF8CHAR = CK_BYTE;
+pub type CK_UTF8CHAR = CK_BYTE;
 #[allow(non_camel_case_types)]
-type CK_BBOOL = CK_BYTE;
+pub type CK_BBOOL = CK_BYTE;
 #[allow(non_camel_case_types)]
-type CK_ULONG = usize;
+pub type CK_ULONG = usize;
 #[allow(non_camel_case_types)]
-type CK_ULONG_PTR = *const CK_ULONG;
+pub type CK_ULONG_PTR = *const CK_ULONG;
 #[allow(non_camel_case_types)]
-type CK_LONG = isize;
-// TODO: enums here
+pub type CK_LONG = isize;
 #[allow(non_camel_case_types)]
-type CK_FLAGS = CK_ULONG;
+pub type CK_FLAGS = CK_ULONG;
 #[allow(non_camel_case_types)]
-type CK_RV = CK_ULONG;
+pub type CK_RV = CK_ULONG;
 #[allow(non_camel_case_types)]
-type CK_SLOT_ID = CK_ULONG;
+pub type CK_SLOT_ID = CK_ULONG;
 #[allow(non_camel_case_types)]
-type CK_SLOT_ID_PTR = *const CK_SLOT_ID;
+pub type CK_SLOT_ID_PTR = *const CK_SLOT_ID;
 
 #[allow(non_camel_case_types, non_snake_case)]
 #[derive(Debug)]
 #[repr(u8)]
-enum CK_VOID {
+pub enum CK_VOID {
+    #[doc(hidden)]
     __Variant1,
+    #[doc(hidden)]
     __Variant2,
 }
 
 // TODO: in rust we could protect more with *const in a lot of cases
 #[allow(non_camel_case_types, non_snake_case)]
-type CK_VOID_PTR = *const CK_VOID;
+pub type CK_VOID_PTR = *const CK_VOID;
 
 #[allow(non_camel_case_types, non_snake_case)]
-type CK_VOID_PTR_PTR = *const CK_VOID_PTR;
+pub type CK_VOID_PTR_PTR = *const CK_VOID_PTR;
 
 #[allow(non_camel_case_types, non_snake_case)]
 #[derive(Debug)]
 #[repr(C)]
-struct CK_VERSION {
-  major: CK_BYTE,  /* integer portion of version number */
-  minor: CK_BYTE,   /* 1/100ths portion of version number */
+pub struct CK_VERSION {
+  pub major: CK_BYTE,  /* integer portion of version number */
+  pub minor: CK_BYTE,   /* 1/100ths portion of version number */
 }
 
 #[allow(non_camel_case_types)]
-type CK_CREATEMUTEX = Option<extern "C" fn(CK_VOID_PTR_PTR) -> CK_RV>;
+pub type CK_CREATEMUTEX = Option<extern "C" fn(CK_VOID_PTR_PTR) -> CK_RV>;
 #[allow(non_camel_case_types)]
-type CK_DESTROYMUTEX = Option<extern "C" fn(CK_VOID_PTR) -> CK_RV>;
+pub type CK_DESTROYMUTEX = Option<extern "C" fn(CK_VOID_PTR) -> CK_RV>;
 #[allow(non_camel_case_types)]
-type CK_LOCKMUTEX = Option<extern "C" fn(CK_VOID_PTR) -> CK_RV>;
+pub type CK_LOCKMUTEX = Option<extern "C" fn(CK_VOID_PTR) -> CK_RV>;
 #[allow(non_camel_case_types)]
-type CK_UNLOCKMUTEX = Option<extern "C" fn(CK_VOID_PTR) -> CK_RV>;
+pub type CK_UNLOCKMUTEX = Option<extern "C" fn(CK_VOID_PTR) -> CK_RV>;
 
 #[allow(non_camel_case_types, non_snake_case)]
 #[derive(Debug)]
 #[repr(C)]
-struct CK_C_INITIALIZE_ARGS {
-  CreateMutex: CK_CREATEMUTEX,
-  DestroyMutex: CK_DESTROYMUTEX,
-  LockMutex: CK_LOCKMUTEX,
-  UnlockMutex: CK_UNLOCKMUTEX,
-  flags: u32,
-  pReserved: CK_VOID_PTR,
+pub struct CK_C_INITIALIZE_ARGS {
+  pub CreateMutex: CK_CREATEMUTEX,
+  pub DestroyMutex: CK_DESTROYMUTEX,
+  pub LockMutex: CK_LOCKMUTEX,
+  pub UnlockMutex: CK_UNLOCKMUTEX,
+  pub flags: CK_FLAGS,
+  pub pReserved: CK_VOID_PTR,
 }
 
 impl CK_C_INITIALIZE_ARGS {
-    fn new() -> CK_C_INITIALIZE_ARGS {
+    pub fn new() -> CK_C_INITIALIZE_ARGS {
         CK_C_INITIALIZE_ARGS {
-            flags: 0,
+            flags: CKF_OS_LOCKING_OK,
             CreateMutex: None,
             DestroyMutex: None,
             LockMutex: None,
@@ -85,25 +86,28 @@ impl CK_C_INITIALIZE_ARGS {
     }
 }
 
+pub const CKF_LIBRARY_CANT_CREATE_OS_THREADS: CK_FLAGS = 0x00000001;
+pub const CKF_OS_LOCKING_OK: CK_FLAGS                  = 0x00000002;
+
 #[allow(non_camel_case_types)]
-type CK_C_INITIALIZE_ARGS_PTR = *const CK_C_INITIALIZE_ARGS;
+pub type CK_C_INITIALIZE_ARGS_PTR = *const CK_C_INITIALIZE_ARGS;
 
 #[allow(non_camel_case_types, non_snake_case)]
 #[derive(Debug)]
 #[repr(C)]
-struct CK_INFO {
+pub struct CK_INFO {
   /* manufacturerID and libraryDecription have been changed from
    * CK_CHAR to CK_UTF8CHAR for v2.10 */
-  cryptokiVersion: CK_VERSION,              /* Cryptoki interface ver */
-  manufacturerID: [CK_UTF8CHAR; 32],        /* blank padded */
-  flags: CK_FLAGS,                          /* must be zero */
+  pub cryptokiVersion: CK_VERSION,              /* Cryptoki interface ver */
+  pub manufacturerID: [CK_UTF8CHAR; 32],        /* blank padded */
+  pub flags: CK_FLAGS,                          /* must be zero */
 
-  libraryDescription: [CK_UTF8CHAR; 32],    /* blank padded */
-  libraryVersion: CK_VERSION,               /* version of library */
+  pub libraryDescription: [CK_UTF8CHAR; 32],    /* blank padded */
+  pub libraryVersion: CK_VERSION,               /* version of library */
 }
 
 impl CK_INFO {
-    fn new() -> CK_INFO {
+    pub fn new() -> CK_INFO {
         CK_INFO {
             cryptokiVersion: CK_VERSION { major: 0, minor: 0 },
             manufacturerID: [0; 32],
@@ -115,110 +119,125 @@ impl CK_INFO {
 }
 
 #[allow(non_camel_case_types)]
-type CK_INFO_PTR = *const CK_INFO;
+pub type CK_INFO_PTR = *const CK_INFO;
 
 #[allow(non_camel_case_types, non_snake_case)]
 #[repr(C)]
-struct CK_SLOT_INFO {
+pub struct CK_SLOT_INFO {
   /* slotDescription and manufacturerID have been changed from
    * CK_CHAR to CK_UTF8CHAR for v2.10 */
-  slotDescription: [CK_UTF8CHAR; 64],    /* blank padded */
-  manufacturerID: [CK_UTF8CHAR; 32],     /* blank padded */
-  flags: CK_FLAGS,
+  pub slotDescription: [CK_UTF8CHAR; 64],    /* blank padded */
+  pub manufacturerID: [CK_UTF8CHAR; 32],     /* blank padded */
+  pub flags: CK_FLAGS,
 
-  hardwareVersion: CK_VERSION,  /* version of hardware */
-  firmwareVersion: CK_VERSION,  /* version of firmware */
+  pub hardwareVersion: CK_VERSION,  /* version of hardware */
+  pub firmwareVersion: CK_VERSION,  /* version of firmware */
+}
+
+impl std::fmt::Debug for CK_SLOT_INFO {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let sd = {
+            &self.slotDescription[..].fmt(fmt)
+        };
+        fmt.debug_struct("CK_SLOT_INFO")
+            .field("slotDescription", &sd)
+            .field("manufacturerID", &self.manufacturerID)
+            .field("flags", &self.flags)
+            .field("hardwareVersion", &self.hardwareVersion)
+            .field("firmwareVersion", &self.firmwareVersion)
+            .finish()
+    }
 }
 
 #[allow(non_camel_case_types)]
-type CK_SLOT_INFO_PTR = *const CK_INFO;
+pub type CK_SLOT_INFO_PTR = *const CK_INFO;
 
 #[allow(non_camel_case_types, non_snake_case)]
 #[derive(Debug)]
 #[repr(C)]
-struct CK_TOKEN_INFO {
+pub struct CK_TOKEN_INFO {
   /* label, manufacturerID, and model have been changed from
    * CK_CHAR to CK_UTF8CHAR for v2.10 */
-  label: [CK_UTF8CHAR; 32],           /* blank padded */
-  manufacturerID: [CK_UTF8CHAR; 32],  /* blank padded */
-  model: [CK_UTF8CHAR; 16],           /* blank padded */
-  serialNumber: [CK_CHAR; 16],        /* blank padded */
-  flags: CK_FLAGS,                    /* see below */
+  pub label: [CK_UTF8CHAR; 32],           /* blank padded */
+  pub manufacturerID: [CK_UTF8CHAR; 32],  /* blank padded */
+  pub model: [CK_UTF8CHAR; 16],           /* blank padded */
+  pub serialNumber: [CK_CHAR; 16],        /* blank padded */
+  pub flags: CK_FLAGS,                    /* see below */
 
-  ulMaxSessionCount: CK_ULONG,     /* max open sessions */
-  ulSessionCount: CK_ULONG,        /* sess. now open */
-  ulMaxRwSessionCount: CK_ULONG,   /* max R/W sessions */
-  ulRwSessionCount: CK_ULONG,      /* R/W sess. now open */
-  ulMaxPinLen: CK_ULONG,           /* in bytes */
-  ulMinPinLen: CK_ULONG,           /* in bytes */
-  ulTotalPublicMemory: CK_ULONG,   /* in bytes */
-  ulFreePublicMemory: CK_ULONG,    /* in bytes */
-  ulTotalPrivateMemory: CK_ULONG,  /* in bytes */
-  ulFreePrivateMemory: CK_ULONG,   /* in bytes */
-  hardwareVersion: CK_VERSION,     /* version of hardware */
-  firmwareVersion: CK_VERSION,     /* version of firmware */
-  utcTime: [CK_CHAR; 16],          /* time */
+  pub ulMaxSessionCount: CK_ULONG,     /* max open sessions */
+  pub ulSessionCount: CK_ULONG,        /* sess. now open */
+  pub ulMaxRwSessionCount: CK_ULONG,   /* max R/W sessions */
+  pub ulRwSessionCount: CK_ULONG,      /* R/W sess. now open */
+  pub ulMaxPinLen: CK_ULONG,           /* in bytes */
+  pub ulMinPinLen: CK_ULONG,           /* in bytes */
+  pub ulTotalPublicMemory: CK_ULONG,   /* in bytes */
+  pub ulFreePublicMemory: CK_ULONG,    /* in bytes */
+  pub ulTotalPrivateMemory: CK_ULONG,  /* in bytes */
+  pub ulFreePrivateMemory: CK_ULONG,   /* in bytes */
+  pub hardwareVersion: CK_VERSION,     /* version of hardware */
+  pub firmwareVersion: CK_VERSION,     /* version of firmware */
+  pub utcTime: [CK_CHAR; 16],          /* time */
 }
 
 #[allow(non_camel_case_types)]
-type CK_TOKEN_INFO_PTR = *const CK_TOKEN_INFO;
+pub type CK_TOKEN_INFO_PTR = *const CK_TOKEN_INFO;
 
 #[allow(non_camel_case_types)]
-type CK_MECHANISM_TYPE = CK_ULONG;
+pub type CK_MECHANISM_TYPE = CK_ULONG;
 #[allow(non_camel_case_types)]
-type CK_MECHANISM_TYPE_PTR = *const CK_MECHANISM_TYPE;
+pub type CK_MECHANISM_TYPE_PTR = *const CK_MECHANISM_TYPE;
 
 #[allow(non_camel_case_types, non_snake_case)]
 #[derive(Debug)]
 #[repr(C)]
-struct CK_MECHANISM_INFO {
-    ulMinKeySize: CK_ULONG,
-    ulMaxKeySize: CK_ULONG,
-    flags: CK_FLAGS,
+pub struct CK_MECHANISM_INFO {
+    pub ulMinKeySize: CK_ULONG,
+    pub ulMaxKeySize: CK_ULONG,
+    pub flags: CK_FLAGS,
 }
 
 #[allow(non_camel_case_types)]
-type CK_MECHANISM_INFO_PTR = *const CK_MECHANISM_INFO;
+pub type CK_MECHANISM_INFO_PTR = *const CK_MECHANISM_INFO;
 
 #[allow(non_camel_case_types)]
-type C_Initialize = extern "C" fn(CK_C_INITIALIZE_ARGS_PTR) -> CK_RV;
+pub type C_Initialize = extern "C" fn(CK_C_INITIALIZE_ARGS_PTR) -> CK_RV;
 #[allow(non_camel_case_types)]
-type C_Finalize = extern "C" fn(CK_VOID_PTR) -> CK_RV;
+pub type C_Finalize = extern "C" fn(CK_VOID_PTR) -> CK_RV;
 #[allow(non_camel_case_types)]
-type C_GetInfo = extern "C" fn(CK_INFO_PTR) -> CK_RV;
+pub type C_GetInfo = extern "C" fn(CK_INFO_PTR) -> CK_RV;
 #[allow(non_camel_case_types)]
-type C_GetFunctionList = extern "C" fn(CK_FUNCTION_LIST_PTR_PTR) -> CK_RV;
+pub type C_GetFunctionList = extern "C" fn(CK_FUNCTION_LIST_PTR_PTR) -> CK_RV;
 #[allow(non_camel_case_types)]
-type C_GetSlotList = extern "C" fn(CK_BBOOL, CK_SLOT_ID_PTR, CK_ULONG_PTR) -> CK_RV;
+pub type C_GetSlotList = extern "C" fn(CK_BBOOL, CK_SLOT_ID_PTR, CK_ULONG_PTR) -> CK_RV;
 #[allow(non_camel_case_types)]
-type C_GetSlotInfo = extern "C" fn(CK_SLOT_ID, CK_SLOT_INFO_PTR) -> CK_RV;
+pub type C_GetSlotInfo = extern "C" fn(CK_SLOT_ID, CK_SLOT_INFO_PTR) -> CK_RV;
 #[allow(non_camel_case_types)]
-type C_GetTokenInfo = extern "C" fn(CK_SLOT_ID, CK_TOKEN_INFO_PTR) -> CK_RV;
+pub type C_GetTokenInfo = extern "C" fn(CK_SLOT_ID, CK_TOKEN_INFO_PTR) -> CK_RV;
 #[allow(non_camel_case_types)]
-type C_GetMechanismList = extern "C" fn(CK_SLOT_ID, CK_MECHANISM_TYPE_PTR, CK_ULONG_PTR) -> CK_RV;
+pub type C_GetMechanismList = extern "C" fn(CK_SLOT_ID, CK_MECHANISM_TYPE_PTR, CK_ULONG_PTR) -> CK_RV;
 #[allow(non_camel_case_types)]
-type C_GetMechanismInfo = extern "C" fn(CK_SLOT_ID, CK_MECHANISM_TYPE, CK_MECHANISM_INFO_PTR) -> CK_RV;
+pub type C_GetMechanismInfo = extern "C" fn(CK_SLOT_ID, CK_MECHANISM_TYPE, CK_MECHANISM_INFO_PTR) -> CK_RV;
 
 #[allow(non_camel_case_types, non_snake_case)]
 #[derive(Debug)]
 #[repr(C)]
-struct CK_FUNCTION_LIST {
-    version: CK_VERSION,
-    C_Initialize: Option<C_Initialize>,
-    C_Finalize: Option<C_Finalize>,
-    C_GetInfo: Option<C_GetInfo>,
-    C_GetFunctionList: Option<C_GetFunctionList>,
-    C_GetSlotList: Option<C_GetSlotList>,
-    C_GetSlotInfo: Option<C_GetSlotInfo>,
-    C_GetTokenInfo: Option<C_GetTokenInfo>,
-    C_GetMechanismList: Option<C_GetMechanismList>,
-    C_GetMechanismInfo: Option<C_GetMechanismInfo>,
+pub struct CK_FUNCTION_LIST {
+    pub version: CK_VERSION,
+    pub C_Initialize: Option<C_Initialize>,
+    pub C_Finalize: Option<C_Finalize>,
+    pub C_GetInfo: Option<C_GetInfo>,
+    pub C_GetFunctionList: Option<C_GetFunctionList>,
+    pub C_GetSlotList: Option<C_GetSlotList>,
+    pub C_GetSlotInfo: Option<C_GetSlotInfo>,
+    pub C_GetTokenInfo: Option<C_GetTokenInfo>,
+    pub C_GetMechanismList: Option<C_GetMechanismList>,
+    pub C_GetMechanismInfo: Option<C_GetMechanismInfo>,
 }
 
 #[allow(non_camel_case_types)]
-type CK_FUNCTION_LIST_PTR = *const CK_FUNCTION_LIST;
+pub type CK_FUNCTION_LIST_PTR = *const CK_FUNCTION_LIST;
 #[allow(non_camel_case_types)]
-type CK_FUNCTION_LIST_PTR_PTR = *const CK_FUNCTION_LIST_PTR;
+pub type CK_FUNCTION_LIST_PTR_PTR = *const CK_FUNCTION_LIST_PTR;
 
 #[derive(Debug)]
 pub enum Error {
@@ -238,7 +257,7 @@ impl std::fmt::Display for Error {
         match *self {
             Error::Io(ref err) => write!(f, "IO error: {}", err),
             Error::Module(ref err) => write!(f, "PKCS#11 Module error: {}", err),
-            Error::Pkcs11(ref err) => write!(f, "PKCS#11 error: {}", err),
+            Error::Pkcs11(ref err) => write!(f, "PKCS#11 error: 0x{:x}", err),
         }
     }
 }
@@ -271,6 +290,10 @@ impl Ctx {
                     err => return Err(Error::Pkcs11(err)),
                 }
             }
+
+            // TODO: The following part is a bit awkward: we want to ensure that every pointer was indeed set,
+            // so instead of matching in the function calls, we do it now, and add them explicitly to the Ctx
+            // There is an interesting nightly feature for '?' support on Option (std::option::NoneError).
 
             let c_initialize = match (*list).C_Initialize {
                 Some(func) => func,
@@ -333,15 +356,21 @@ impl Ctx {
         }
     }
 
+    pub fn new_and_initialize(filename: &'static str) -> Result<Ctx, Error> {
+        let mut ctx = Ctx::new(filename)?;
+        ctx.initialize(None)?;
+        Ok(ctx)
+    }
+
     pub fn is_initialized(&self) -> bool {
         self._is_initialized
     }
 
-    pub fn initialize(&mut self) -> Result<(), Error> {
+    pub fn initialize(&mut self, init_args: Option<CK_C_INITIALIZE_ARGS>) -> Result<(), Error> {
         if self._is_initialized {
             return Err(Error::Module("module already initialized"))
         }
-        match (self.C_Initialize)(&CK_C_INITIALIZE_ARGS::new()) {
+        match (self.C_Initialize)(&init_args.unwrap_or(CK_C_INITIALIZE_ARGS::new())) {
             0 => {
                 self._is_initialized = true;
                 Ok(())
@@ -363,14 +392,14 @@ impl Ctx {
         }
     }
 
-    pub fn get_info(&self) -> () {
-        //unsafe {
-            println!("Ctx: {:?}", self);
-            let mut info = Box::new(CK_INFO::new());
-            let rv = (self.C_GetInfo)(&mut *info);
-            println!("Rv: {:?}, Info: {:?}", rv, info);
-        //}
-        ()
+    pub fn get_info(&self) -> Result<CK_INFO, Error> {
+        let mut info = Box::new(CK_INFO::new());
+        match (self.C_GetInfo)(&mut *info) {
+            0 => {
+                Ok(*info)
+            },
+            err => Err(Error::Pkcs11(err)),
+        }
     }
 }
 
@@ -390,10 +419,9 @@ mod tests {
 
     #[test]
     fn can_open_context() {
-        let mut ctx = Ctx::new("/usr/local/lib/softhsm/libsofthsm2.so").unwrap();
-        ctx.initialize().unwrap();
-        ctx.get_info();
-        //println!("{:?}", res);
+        let ctx = Ctx::new_and_initialize("/usr/local/lib/softhsm/libsofthsm2.so").unwrap();
+        let info = ctx.get_info().unwrap();
+        println!("{:#?}", info);
         //assert_eq!(res.is_ok(), true);
     }
 }
