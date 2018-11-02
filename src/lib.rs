d22cb45ce31fe4f09facb8591eb0e767479be289
Marcus Heese <marcus.heese@gmail.com>
2018-11-01 23:07:09 -0700

    fixing missing 'as CK_ULONG' which breaks on Windows

--- src/lib.rs
+++ src/lib.rs
@@ -887,7 +887,7 @@ impl Ctx {
     match (self.C_Sign)(session, data.as_slice().as_ptr(), data.len() as CK_ULONG, ptr::null(), &mut signatureLen) {
       CKR_OK => {
         let mut signature: Vec<CK_BYTE> = Vec::with_capacity(signatureLen as usize);
-        match (self.C_Sign)(session, data.as_slice().as_ptr(), data.len(), signature.as_slice().as_ptr(), &signatureLen) {
+        match (self.C_Sign)(session, data.as_slice().as_ptr(), data.len() as CK_ULONG, signature.as_slice().as_ptr(), &signatureLen) {
           CKR_OK => {
             unsafe {
               signature.set_len(signatureLen as usize);
