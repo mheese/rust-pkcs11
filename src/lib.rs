1f5df74aacef8b069cf37c76bad95c0c799e4aee
Jade Harley <jade@bytes.moe>
2020-02-10 20:07:03 -0600

    Avoided unnecessary line change

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #![allow(non_camel_case_types, non_snake_case)]
+
 extern crate libloading;
 extern crate num_bigint;
 
