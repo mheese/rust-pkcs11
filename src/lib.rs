592653813a059e190f7cd2522cea11120b6f9836
Marcus Heese <marcus.heese@gmail.com>
2020-04-04 11:14:26 -0700

    fixing some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#![allow(non_camel_case_types, non_snake_case)]
+#![allow(non_camel_case_types, non_snake_case, clippy::unreadable_literal)]
 
 extern crate libloading;
 extern crate num_bigint;
